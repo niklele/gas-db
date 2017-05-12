cd7b2171a1ac46f147199ec66a04a899f73476c8
Nikhil Lele <nik.lele@gmail.com>
2017-05-12 13:00:00 -0700

    scrape many locations

--- scrape.rb
+++ scrape.rb
@@ -5,14 +5,36 @@ require 'dotenv/load'
 require 'date'
 require 'time'
 require 'sequel'
+require 'uri'
 
+# stations that can't be parsed properly bc of a problem on the website
+$blacklist_stations = [4068]
 
+$fuel_type = {
+    'regular' => 'A',
+    'midgrade' => 'B',
+    'premium' => 'C',
+    'diesel' => 'D'
+}
+
+$locations = ['Atherton',
+              'Cupertino',
+              'East Palo Alto',
+              'Los Altos',
+              'Menlo Park',
+              'Mountain View',
+              'Palo Alto',
+              'Santa Clara',
+              'Saratoga',
+              'Sunnyvale']
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
 def parseStation(name, location, station_id)
 
-    url = "http://www.sanfrangasprices.com/#{name}_Gas_Stations/#{location}/#{station_id}/index.aspx"
+    puts "scraping #{name} station in #{location} with id #{station_id}"
+
+    url = URI.escape("http://www.sanfrangasprices.com/#{name}_Gas_Stations/#{location}/#{station_id}/index.aspx")
 
     page = Nokogiri::HTML(HTTParty.get(url))
 
@@ -24,7 +46,12 @@ def parseStation(name, location, station_id)
 
     address, phone = info.css('dd').text.split(/[Pp]hone:/)
     data[:address] = address.strip
-    data[:phone] = phone.strip
+
+    if phone.nil?
+        data[:phone] = ''
+    else
+        data[:phone] = phone.strip
+    end
 
     puts data
 
@@ -34,26 +61,20 @@ def parseStation(name, location, station_id)
                              :name => data[:name],
                              :address => data[:address],
                              :phone => data[:phone])
-    rescue
+    rescue => e
+        puts e
         # nothing
     end
 
 end
 
-
-
 def parseLocation(location, fuel)
 
-    fuel_type = {
-        :regular => :A,
-        :midgrade => :B,
-        :premium => :C,
-        :diesel => :D
-    }
+    puts "scraping prices in #{location} for #{fuel} fuel"
 
-    type = fuel_type[fuel]
+    type = $fuel_type[fuel]
 
-    url = "http://www.sanfrangasprices.com/GasPriceSearch.aspx?fuel=#{type}&typ=adv&srch=1&state=CA&area=#{location}&site=SanFran,SanJose,California&tme_limit=4"
+    url = URI.escape("http://www.sanfrangasprices.com/GasPriceSearch.aspx?fuel=#{type}&typ=adv&srch=1&state=CA&area=#{location}&site=SanFran,SanJose,California&tme_limit=4")
 
     page = Nokogiri::HTML(HTTParty.get(url))
     rows = page.xpath('//*[@id="pp_table"]/table/tbody/tr')
@@ -67,6 +88,11 @@ def parseLocation(location, fuel)
         data[:price] = Float(p_price.text)
         data[:station_id] = Integer(p_price[0]['id'].split('_').last)
 
+        if $blacklist_stations.include? data[:station_id]
+            puts "skipping blacklisted station #{data[:station_id]}"
+            next
+        end
+
         address = row.css('.address')
         data[:name] = address.css('a').text.strip
         data[:address] = address.css('dd').text.strip
@@ -76,25 +102,39 @@ def parseLocation(location, fuel)
 
         puts data
 
+        noStation = false
         begin
             DB.transaction do
-                while (DB[:stations].where(:station_id => data[:station_id]).count < 1)
+                if (DB[:stations].where(:station_id => data[:station_id]).count < 1)
+                    noStation = true
                     parseStation(data[:name], location, data[:station_id])
                 end
 
                 DB[:prices].insert(:station_id => data[:station_id],
                                    :collected => collected,
                                    :reported => data[:reported],
-                                   :type => 'regular',
+                                   :type => fuel, # real name not A/B/C/D
                                    :price => data[:price],
                                    :user => data[:user])
             end
 
-        rescue
-            # nothing
+        rescue => e
+            puts e
+            if noStation
+                retry
+            end
+
         end
     }
 
 end
 
-parseLocation('Sunnyvale', :regular)
+$locations.each { |loc|
+    $fuel_type.keys.each { |fuel|
+
+        parseLocation(loc, fuel)
+
+        # rate limiting
+        sleep(1)
+    }
+}
